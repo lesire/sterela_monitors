9c6696f87d2e076a6cf6571457561d3f7d754af1
Thierry Sotiropoulos <thierry.sotiropoulos@laas.fr>
2016-11-08 17:23:54 +0100

    corrections en rapport avec l'emplacement du laser

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -8,10 +8,10 @@ from sensor_msgs.msg import LaserScan
 
 
 # Enum for describing the states
-class State(enum):
-    SAFE = 2
+class State(Enum):
+    SAFE = 2.0
     WARNING = 0.5
-    COLLISION = 0
+    COLLISION = 0.0
 
 
 class Monitor:
@@ -47,11 +47,12 @@ class Monitor:
         self.dx1 = 50.0
         self.dx2_low = 50.0
 
+
         # Loop over the scan echos
         for i in range(len(scan.ranges)):
             distance = scan.ranges[i]
             # If range has inconsistent values, discard it
-            if distance < scan.range_min or distance > scan.range_max:
+            if distance < scan.range_min or distance > 5.0:
                 continue
 
             # else, compute angle, delta x, delta y
@@ -60,11 +61,11 @@ class Monitor:
             dx = distance * cos(angle)
 
             # Determine the occupied zone
-            if (-self.platform_width) / 2.0 < dy < self.platform_width / 2.0:
+            if -self.platform_width < dy < 0.0:
                 self.zone1 = True
                 if self.dx1 > dx:
                     self.dx1 = dx
-            elif self.platform_width / 2.0 < dy < self.platform_width / 2.0 + self.arm_len:
+            elif 0.0 < dy < self.arm_len:
                 self.zone2_low = True
                 if self.dx2_low > dx:
                     self.dx2_low = dx
@@ -85,7 +86,7 @@ class Monitor:
         for i in range(len(scan.ranges)):
             distance = scan.ranges[i]
             # If range has inconsistent values, discard it
-            if distance < scan.range_min or distance > scan.range_max:
+            if distance < scan.range_min or distance > 5.0:
                 continue
 
             # else, compute angle, delta x, delta y
@@ -104,20 +105,25 @@ class Monitor:
 
     def command_cb(self, cmd):
         # Compute the flag
-        dx2 = 50.0
-        if self.zone2:
-            dx2 = min(self.dx2_low, self.dx2_high-self.platform_len/2)
-        if self.dx1 > State.SAFE and dx2 > State.SAFE:
+        # dx2 = 50.0
+        # if self.zone2_high or self.zone2_low:
+        #     dx2 = min(self.dx2_low, self.dx2_high-self.platform_len/2)
+        dx2 = self.dx2_high-self.platform_len/2
+        if self.dx1 >= State.SAFE and dx2 >= State.SAFE:
             self.state=State.SAFE
-        if self.zone1 and State.SAFE < self.dx1 < State.WARNING or self.zone2_high and self.zone2_low and State.SAFE < dx2 < State.WARNING:
+        if State.SAFE < self.dx1 < State.WARNING or State.SAFE < dx2 < State.WARNING:
             self.state = State.WARNING
-        if State.COLLISION < self.dx1 < State.WARNING or  self.zone2_high and self.zone2_low and State.COLLISION < dx2 < State.WARNING:
+        if State.COLLISION < self.dx1 < State.WARNING or State.COLLISION < dx2 < State.WARNING:
             self.state = State.COLLISION
-
+            
         rospy.loginfo("State: " + str(self.state))
         # Use the status of zones to decide, e.g.
         if self.state == State.WARNING or self.state == State.COLLISION:
             # Send stop!
+            if cmd.linear.x < 0.01:
+                rospy.loginfo("Stopped by controller")
+            else:
+                rospy.logwarn("MONITOR ACTIVATED")
             self.command_publisher.publish(Twist())
         else:
             # Do nothing, i.e. forward the real Command
